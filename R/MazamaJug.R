eba45b79af2d1715dafb98dc9efa46fd8b58ca1a
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-09-16 18:20:13 -0700

    first skeletal checkin

---
+++ R/MazamaJug.R
@@ -0,0 +1,7 @@
+#' @docType package
+#' @name MazamaJug
+#' @title A Simple Web Framework for R.
+#' @description A web framework mimicking the functionality found in python Flask
+#' including basic functionality for handling web GET and PUT requests. This
+#' package is a ground-up rewrite of the original "jug" package by Bart Smeets.
+NULL
---
+++ R/jug.R
@@ -0,0 +1,67 @@
+#
+# TODO:  How to properly document R6 classes?
+#
+Jug <- R6::R6Class("Jug",
+
+  # Define public interface
+  public = list(
+    
+    # TODO:  Explain this field
+    request_handler = NULL,
+    
+    # TODO:  Explain this field
+    daemon_obj = NULL,
+    
+    # TODO:  Method to ???
+    app_definition = function() {
+      list(
+        call = function(req) {
+          self$request_handler$invoke(req)
+        },
+        onWSOpen = function(ws) {
+          ws$onMessage(function(binary, message) {
+            ws$send(self$request_handler$invoke(ws$request,
+                                                ws_message = message,
+                                                ws_binary = binary)
+            )
+          })
+        }
+      )
+    },
+    
+    # TODO:  Method to ???
+    add_collected_middelware = function(collector) {
+      self$request_handler$add_middleware(collector$request_handler$middlewares)
+    },
+    
+    # TODO:  Method to ???
+    initialize = function() {
+      self$request_handler = RequestHandler$new()
+      options("jug.verbose" = FALSE) # set early for testing purposes were serve_it isn't called
+    },
+    
+    # TODO:  Method to ???
+    start = function(host, port, daemonized) {
+      if ( daemonized ) {
+        self$daemon_obj<-
+          httpuv::startDaemonizedServer(host, port, self$app_definition())
+      } else {
+        httpuv::runServer(host, port, self$app_definition())
+      }
+      
+    },
+    
+    # TODO:  Method to ???
+    print = function(...) {
+      cat("A Jug instance with ",
+          length(self$request_handler$middlewares),
+          " middlewares attached\n", sep = "")
+      invisible(self)
+    }
+    
+  ) # END public interface definition
+  
+) # END R6 class definition
+
+
+
