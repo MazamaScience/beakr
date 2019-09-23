c5a5aee6f46e97efc5c9d06a9c0f8bc7759e1b86
Hans Martin <hansmrtn@gmail.com>
2019-09-23 16:21:35 -0700

    reduce, reuse, recycle

--- R/Beakr.R
+++ R/Beakr.R
@@ -1,3 +1,11 @@
+#' @docType package
+#' @name Beakr
+#' @title A Simple Web Framework for R.
+#' @description A web framework mimicking the functionality found in python Flask
+#' including basic functionality for handling web GET and PUT requests. This
+#' package is a ground-up rewrite of the original "jug" package by Bart Smeets.
+NULL
+
 #' The Beakr class
 #'
 #' @importFrom magrittr %>%
