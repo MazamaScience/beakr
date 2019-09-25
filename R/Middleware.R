edb66decb77480867476237e7b1eb0317e1f7331
Hans Martin <hansmrtn@gmail.com>
2019-09-25 14:13:59 -0700

    moved helpers to utils

--- R/Middleware.R
+++ R/Middleware.R
@@ -18,52 +18,57 @@ Middleware <-
     )
   )
 
-#' Internal function to add middleware
+#' #' Internal function to add middleware
+#' #'
+#' #' @param beakr
+#' #' @param FUN
+#' #' @param path
+#' #' @param method
+#' #' @param websocket
+#' #'
+#' #' @return
 #'
-#' @param beakr
-#' @param FUN
-#' @param path
-#' @param method
-#' @param websocket
+#' addMiddleware <-
+#'   function(beakr, FUN, path = NULL, method = NULL, websocket = FALSE) {
+#'     method <- ifelse(!is.null(method), toupper(method), NULL)
+#'     # Create new middleware
+#'     mw <- Middleware$new(FUN, path, method, websocket)
+#'     # Add the middleware
+#'     beakr$requestHandler$addMiddleware(mw)
+#'     return(beakr)
+#'   }
 #'
-#' @return
-
-addMiddleware <-
-  function(beakr, FUN, path = NULL, method = NULL, websocket = FALSE) {
-    method <- ifelse(!is.null(method), toupper(method), NULL)
-    # Create new middleware
-    mw <- Middleware$new(FUN, path, method, websocket)
-    # Add the middleware
-    beakr$requestHandler$addMiddleware(mw)
-    return(beakr)
-  }
-
-#' GET-binding middleware
+#' #' GET-binding middleware
+#' #'
+#' #' @param object
+#' #' @param ...
+#' #'
+#' #' @return
+#' #' @export
+#' #'
+#' get <- function(beakr, ...) {
+#'   return(UseMethod("get"))
+#' }
 #'
-#' @param object
-#' @param ...
+#' #' @export
+#' get.default <- function(beakr, ...) {
+#'   return(base::get(beakr, ...))
+#' }
 #'
-#' @return
-#' @export
 #'
-get <- function(beakr, ...) {
-  return(UseMethod("get"))
-}
-
-#' @export
-get.default <- function(beakr, ...) {
-  return(base::get(beakr, ...))
-}
-
-
-#' @describeIn GET-binding middleware
-#' @export
-get.Beakr<-function(beakr, ...){
-  funcs<-list(...)
-  path<-funcs[[1]]
-  funcs<-funcs[-1]
-
-  lapply(funcs, function(mw_func) addMiddleware(beakr, mw_func, path, method="GET"))
-
-  return(beakr)
-}
+#' #' @describeIn GET-binding middleware
+#' #' @export
+#' get.Beakr <- function(beakr, ...) {
+#'   FUNS <- list(...)
+#'   path <- FUNS[[1]]
+#'   FUNS <- FUNS[-1]
+#'
+#'   lapply(
+#'     X   = FUNS,
+#'     FUN = function(mw_FUN) {
+#'       addMiddleware(beakr, mw_FUN, path, method = "GET")
+#'     }
+#'   )
+#'
+#'   return(beakr)
+#' }
