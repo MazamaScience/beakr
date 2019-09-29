7c97408725e01399792d03bd0d74f841d50c95d1
Hans Martin <hansmrtn@gmail.com>
2019-09-28 18:51:17 -0700

    updated docs

--- R/Error.R
+++ R/Error.R
@@ -1,15 +1,42 @@
-#' Error Response Class
+#' Error class
+#'
+#' A \code{Error} class object represents the state and handling of instance
+#' or middleware errors.
+#'
+#' @usage NULL
+#'
+#' @format NULL
+#'
+#' @section Fields:
+#'
+#' \describe{
+#'   \item{\code{errors}}{
+#'   Returns a list of errors, if any.
+#'   }
+#'   \item{\code{occured}}{
+#'   Returns TRUE if any error has occurred, FALSE otherwise.
+#'   }
+#' }
+#'
+#' @section Methods:
+#'
+#' \describe{
+#'   \item{\code{set(err)}}{
+#'   Sets an error.
+#'   }
+#' }
+#'
+#' @seealso \code{\link{Beakr}} and \code{\link{Middleware}}
+#' @keywords internal
 Error <-
   R6::R6Class(
     classname = "Error",
     public = list(
       errors = c(),
-      # Set the errors if occurs
       set = function(err) {
         self$errors <- c(self$errors, as.character(err))
       }
     ),
-    # Show the occured errors, if any
     active = list(
       occurred = function() {
         return(length(self$errors) > 0)
--- R/Listener.R
+++ R/Listener.R
@@ -1,5 +1,32 @@
 #' Listener class
-
+#'
+#' The \code{Listener} class listens...to things.
+#'
+#' @usage NULL
+#'
+#' @format NULL
+#'
+#' @section Fields:
+#'
+#' \describe{
+#'   \item{\code{FUN}}{
+#'   Returns function response.
+#'   }
+#'   \item{\code{event}}{
+#'   Returns event type.
+#'   }
+#' }
+#'
+#' @section Methods:
+#'
+#' \describe{
+#'   \item{\code{initialize(FUN, event)}}{
+#'   Sets instance object function and event state.
+#'   }
+#' }
+#'
+#' @seealso \code{\link{RequestHandler}} and \code{\link{Error}}
+#' @keywords internal
 Listener <-
   R6::R6Class(
     classname = "Listener",
