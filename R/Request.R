a1b8793c0b3bfdf7297050f8e692bd4cfa5784ab
Hans Martin <hansmrtn@gmail.com>
2019-09-29 13:16:56 -0700

    docs

--- R/Request.R
+++ R/Request.R
@@ -1,4 +1,40 @@
-#' Request Object Class
+#' Request Class
+#'
+#' The \code{Middleware} object represents middleware functions that have access
+#' to the (\code{req}) object, response object (\code{res}) and (\code{err})
+#' object in the request-response cycle via \code{RequestHandler}.
+#'
+#' @usage NULL
+#'
+#' @format NULL
+#'
+#' @section Methods:
+#'
+#' \describe{
+#'   \item{\code{parameters}}{
+#'   A list if parameters.
+#'   }
+#'   \item{\code{}}{
+#'   Returns the function response.
+#'   }
+#'   \item{\code{method}}{
+#'   Returns the HTTP method for the middleware, i.e. "GET", "POST", etc.
+#'   }
+#'   \item{\code{protocol}}{
+#'   Returns the protocol, "http" or "websocket".
+#'   }
+#' }
+#'
+#' @section Methods:
+#'
+#' \describe{
+#'   \item{\code{initialize(FUN, path, method, websocket)}}{
+#'   Initializes the state of new middleware.
+#'   }
+#' }
+#'
+#' @seealso \code{\link{RequestHandler}} and \code{\link{Middleware}}
+#' @keywords internal
 Request <-
   R6::R6Class(
     classname = "Request",
