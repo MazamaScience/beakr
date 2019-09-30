1dbd85ce7b109fb182a0bfaf5999a85ef76897f9
Hans Martin <hansmrtn@gmail.com>
2019-09-30 13:03:26 -0700

    split utils

---
+++ R/http-methods.R
@@ -0,0 +1,154 @@
+#' GET-binding middleware
+#'
+#' Routes HTTP GET requests to the specified path with the specified callback
+#' functions or middleware.
+#'
+#' @param beakr a beakr instance.
+#' @param path string representing a relative path for which the middleware
+#' is invoked.
+#' @param ... additional middleware/functions.
+#'
+#' @return
+#' @export
+#' @examples
+#' \dontrun{
+#' # Create a beakr instance
+#' server <- beakr() %>%
+#'   get("/", function(req, res, err) { "Hello, world!" })
+#' # Listen for HTTP/WebSocket requests
+#' listen(server, daemonized = TRUE)
+#' }
+get <- function(beakr, path, ...) {
+  return(UseMethod("get"))
+}
+#' Default R get-binding
+#' @export
+get.default <- function(beakr, ...) {
+  return(base::get(beakr, ...))
+}
+#' beakr GET-binding middleware
+#' @export
+get.Beakr <- function(beakr, path, ...) {
+  FUNS <- list(...)
+  lapply(
+    X   = FUNS,
+    FUN = function(middleware_FUN) {
+      routeMiddleware( beakr  = beakr,
+                       FUN    = middleware_FUN,
+                       path   = path,
+                       method = "GET" )
+    }
+  )
+  return(beakr)
+}
+
+#' POST-binding middleware
+#'
+#' Routes HTTP POST requests to the specified path with the specified callback
+#' functions or middleware.
+#'
+#' @param beakr a beakr instance.
+#' @param path string representing a relative path for which the middleware
+#' is invoked.
+#' @param ... additional middleware/functions.
+#'
+#' @return
+#' @export
+#' @examples
+#' \dontrun{
+#' # Create a beakr instance
+#' server <- beakr() %>%
+#'   post("/", function(req, res, err) { "Successful POST request!" })
+#' # Listen for HTTP/WebSocket requests
+#' listen(server)
+#' # In terminal:
+#' $ curl -X POST http://127.0.0.1:8080/
+#' > Successful POST request!
+#' }
+post <- function(beakr, path, ...) {
+  FUNS <- list(...)
+  lapply(
+    X = FUNS,
+    FUN = function(middleware_FUN) {
+      routeMiddleware( beakr  = beakr,
+                       FUN    = middleware_FUN,
+                       path   = path,
+                       method = "POST" )
+    }
+  )
+  return(beakr)
+}
+
+#' PUT-binding middleware
+#'
+#' Routes HTTP PUT requests to the specified path with the specified callback
+#' functions or middleware.
+#'
+#' @param beakr a beakr instance.
+#' @param path string representing a relative path for which the middleware
+#' is invoked.
+#' @param ... additional middleware/functions.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Create a beakr instance
+#' server <- beakr() %>%
+#'   put("/", function(req, res, err) { "Successful PUT request!" })
+#' # Listen for HTTP/WebSocket requests
+#' listen(server)
+#' # In terminal:
+#' $ curl -X PUT http://127.0.0.1:8080/
+#' Successful PUT request!
+#' }
+put <- function(beakr, path, ...) {
+  lapply(
+    X = list(...),
+    FUN = function(middleware_FUN) {
+      routeMiddleware( beakr  = beakr,
+                       FUN    = middleware_FUN,
+                       path   = path,
+                       method = "PUT" )
+    }
+  )
+  return(beakr)
+}
+
+#' DELETE-binding middleware
+#'
+#' Routes HTTP DELETE requests to the specified path with the specified callback
+#' functions or middleware.
+#'
+#' @param beakr a beakr instance.
+#' @param path string representing a relative path for which the middleware
+#' is invoked.
+#' @param ... additional middleware/functions.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Create a beakr instance
+#' server <- beakr() %>%
+#'   delete("/", function(req, res, err) { "Successful DELETE request!" })
+#' # Listen for HTTP/WebSocket requests
+#' listen(server, daemonized = TRUE)
+#' # In terminal:
+#' $ curl -X DELETE http://127.0.0.1:8080/
+#' Successful PUT request!
+#' }
+delete <- function(beakr, path, ...) {
+  lapply(
+    X = list(...),
+    FUN = function(middleware_FUN) {
+      routeMiddleware( beakr  = beakr,
+                       FUN    = middleware_FUN,
+                       path   = path,
+                       method = "DELETE" )
+    }
+  )
+  return(beakr)
+}
